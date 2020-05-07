f607a6519b4e98f9821f426057079ee313c1eec0
Lsong <song940@gmail.com>
2020-05-07 15:04:27 +0800

    add typescript dts

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-import { name, main } from './package.json';
+import { name, main, types, source } from './package.json';
 
-export default {
-  input: './src/index.ts',
-  output: {
-    name,
-    format: 'umd',
-    file: main,
+export default [
+  {
+    input: source,
+    output: {
+      name,
+      file: main,
+      format: 'umd',
+    },
+    plugins: [
+      typescript()
+    ]
   },
-  plugins: [
-    typescript()
-  ]
-}
+  {
+    input: source,
+    output: {
+      file: types,
+    },
+    plugins: [dts()]
+  }
+];
